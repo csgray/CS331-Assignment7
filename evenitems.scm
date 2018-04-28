da5e482265110033a0f65be5bb1f175cb4b7a16b
Corey Gray <csgray@alaska.edu>
2018-04-27 21:58:38 -0800

    Add evenitems.scm. evenitems works for lists with an even number of items.

---
+++ evenitems.scm
@@ -0,0 +1,16 @@
+; evenitems.scm
+; Corey S. Gray
+; 27 April 2018
+;
+; For CS F331 / CSCE A331 Spring 2018
+; Solution to Assignment 7 Exercise C
+
+#lang scheme
+
+; evenitems
+; Takes a list.
+; Returns a list of all items in the given list with even index, where indices start with zero.
+(define (evenitems l)
+  (if (null? l) `()
+  (cons (car l) (evenitems (cdr (cdr l))))))
+  
